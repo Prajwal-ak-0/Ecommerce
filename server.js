130cdbe0a9a40904ed10ce40a7a9d2015f427bf3
Prajwal-ak-0 <prajwalhassan7@gmail.com>
2023-03-24 11:52:20 +0530

    Register Validation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors"
 import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
 
 dotenv.config();
 
@@ -11,8 +12,10 @@ const app=express()
 
 app.use(express.json())
 
+app.use("/api/v1/auth",authRoutes)
+
 app.get('/',(req,res)=>{
-    res.send("<h1>E-Commerce</h1>")
+    res.send("<h1>Prajwal</h1>")
 })
 
 app.listen(process.env.PORT,()=>{
