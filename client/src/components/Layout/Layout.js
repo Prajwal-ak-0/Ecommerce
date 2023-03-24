34b2b7d6c8717234d48fb25b3ccea4e43987c7f0
Prajwal-ak-0 <prajwalhassan7@gmail.com>
2023-03-24 19:04:42 +0530

    RegisterDone

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import {Helmet} from "react-helmet"
+import { Toaster } from 'react-hot-toast'
+const Layout = ({children,title,description,keywords,author}) => {
+  return (
+    <div>
+        <Helmet>
+          <meta charSet='utf-8'/>
+          <meta name='description' content={description} />
+          <meta name='keywords' content={keywords} />
+          <meta name='author' content={author} />
+          <title>{title}</title>
+        </Helmet>
+        <Header/>
+        <main style={{minHeight:"70vh"}}>
+          <Toaster/>
+            {children}
+        </main>
+        <Footer/>
+    </div>
+    
+  )
+}
+
+Layout.defaultProps={
+  title:"ECommerce App",
+  description:"full stack project",
+  keyword:"react node mongodb express",
+  author:"Prajwal A k"
+}
+
+export default Layout
---
+++ client/src/pages/Auth/Register.js
@@ -0,0 +1,81 @@
+import React,{useState} from 'react'
+import Layout from '../../components/Layout/Layout'
+import axios from 'axios'
+import toast from "react-hot-toast";
+import { useNavigate } from 'react-router-dom';
+const Register = () => {
+    const [name,setname]=useState("")
+    const [email,setemail]=useState("")
+    const [password,setpassword]=useState("")
+    const [phone,setphone]=useState("")
+    const [address,setaddress]=useState("")
+    const navigate = useNavigate();
+
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        try {
+            const res=await axios.post("/api/v1/auth/register",{name,email,password,phone,address
+            });
+            if(res.data.success){
+                toast.success(res.data.message);
+                navigate("/login");
+            }
+            else{
+                toast.error(res.data.message);
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something went wrong")
+        }
+    }
+  return (
+    <Layout title="Register">
+        <div className="register">
+            <h1>Register Form</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                <div className="mb-3">
+                    <input type="text" 
+                    value={name}
+                    onChange={(e)=> setname(e.target.value)}
+                    className="form-control" id="exampleInputName1" placeholder='Name'
+                    required/>
+                </div>
+                <div className="mb-3">
+                    <input type="email" 
+                    value={email}
+                    onChange={(e)=> setemail(e.target.value)}
+                    className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email' 
+                    required/>
+                </div>
+                <div className="mb-3">
+                    <input type="password" 
+                    value={password}
+                    onChange={(e)=> setpassword(e.target.value)}
+                    className="form-control" id="exampleInputPassword1" placeholder='Password' 
+                    required
+                    />
+                </div>
+                <div className="mb-3">
+                    <input type="text" 
+                    value={phone}
+                    onChange={(e)=> setphone(e.target.value)}
+                    className="form-control" id="exampleInputPhone1" placeholder='Phone Number'
+                    required />
+                </div>
+                <div className="mb-3">
+                    <input type="text" 
+                    value={address}
+                    onChange={(e)=> setaddress(e.target.value)}
+                    className="form-control" id="exampleInputAddress" placeholder='Address'
+                    required />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </Layout>
+  )
+}
+
+export default Register
